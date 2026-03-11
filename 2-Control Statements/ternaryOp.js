28a7dbbaed29eb893e4d7d66d6417f9cd09ce040
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-11 23:38:36 +0500

    adding ternary operator

---
+++ 2-Control Statements/ternaryOp.js
@@ -0,0 +1,6 @@
+// statement ? true : false
+// a short hand of a two way if - else statement
+let isAdult = true;
+let statement1 = (isAdult)? console.log("You are an Adult") : console.log("You are a minor");
+
+
